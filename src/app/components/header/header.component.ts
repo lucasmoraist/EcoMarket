d6c7bcce390aa4b068bdd89f914ff327f0be5261
lucasmoraist <luksmnt1101@gmail.com>
2024-03-12 16:23:21 -0300

    feat: desenvolvendo cabeçalho do projeto

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  faSearch = faSearch
+}

da443e8abbf50f78ccb9b54ffed6a61c8b79c078
lucasmoraist <luksmnt1101@gmail.com>
2024-03-18 16:37:03 -0300

    feat: correcting syntax

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
+import { GreenSaleComponent } from '../../components/green-sale/green-sale.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent],
+  imports: [BannerComponent, GreenSaleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

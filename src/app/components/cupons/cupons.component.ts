6b7b5b046657f248b345a91d576db95b11c0ad90
lucasmoraist <luksmnt1101@gmail.com>
2024-03-19 16:29:34 -0300

    feat: created cupons component

--- src/app/components/cupons/cupons.component.ts
+++ src/app/components/cupons/cupons.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { Discount } from '../../interface/Discount';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cupons',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cupons.component.html',
   styleUrl: './cupons.component.scss'
 })
 export class CuponsComponent {
+
+  listCupons: Discount[] = [
+    {
+      value: 200,
+      desc: "Compras acima de R$ 350",
+      validate: "21 marc"
+    },
+    {
+      value: 100,
+      desc: "Compras acima de R$ 180",
+      validate: "13 de marc"
+    },
+    {
+      value: 50,
+      desc: "Compras acima de R$ 150",
+      validate: "18 de marc"
+    },{
+      value: 150,
+      desc: "Compras acima de R$ 300",
+      validate: "21 de mar"
+    }
+  ]
 
 }
---
+++ src/app/interface/Discount.ts
@@ -0,0 +1,5 @@
+export interface Discount {
+    value: number,
+    desc: string,
+    validate: string
+}

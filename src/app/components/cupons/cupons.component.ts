d10ce6a38e2b6f98c5170e2cfb30015449935fd2
lucasmoraist <luksmnt1101@gmail.com>
2024-03-19 15:03:17 -0300

    feat: last components

---
+++ src/app/components/cupons/cupons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cupons',
+  standalone: true,
+  imports: [],
+  templateUrl: './cupons.component.html',
+  styleUrl: './cupons.component.scss'
+})
+export class CuponsComponent {
+
+}
